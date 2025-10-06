83a86246fb43c87658c3bdd43cf8012d9132c3c6
Mujtaba Haider <work.mujtabahaider@gmail.com>
2025-10-06 19:19:17 +0500

    Create Dynamicyear.php

---
+++ Dynamicyear.php
@@ -0,0 +1,7 @@
+// Dynamic Copyright Year Shortcode
+// Add this to your theme's functions.php (preferably child theme)
+function dynamic_copyright() {
+    // Replace 'YourSiteName' with your website or client name
+    return '&copy; ' . date('Y') . ' YourSiteName';
+}
+add_shortcode('copyright_year', 'dynamic_copyright');
